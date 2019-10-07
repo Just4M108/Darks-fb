c14300b04de3b27462fd3ebd81d266da6dd53f7e
Just4M108 <55776606+Just4M108@users.noreply.github.com>
2019-10-07 09:46:41 +0700

    Update R13F.py

--- R13F.py
+++ R13F.py
@@ -1,4 +1,4 @@
-#Decompiled by Deray Feat  Ahmad Riswanto 
+#Decompiled by Deray Feat Risky dewantara187
 import os, sys, time, datetime, random, hashlib, re, threading, json, getpass, urllib, requests, mechanize
 from multiprocessing.pool import ThreadPool
 
